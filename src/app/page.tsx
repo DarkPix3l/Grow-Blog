44c132384ae87e2f16ffeeec50f43ee91de4cdcc
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-08-31 12:15:25 +0200

    feat(layout): move header to top level.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./_components/_header/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Header />
       <main className={styles.main}>
         <Image className={styles.logo} src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <ol>
