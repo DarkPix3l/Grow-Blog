f1afc486313c75679291ebc5841556988f17e161
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-09-08 16:13:55 +0200

    Feat: Add article page template and fix header height:
    -made first draft of the Articles page structure;
    - the header component has been updated to use a fixed height, which resolves a layout issue caused by miscalculations with vh units;

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,37 @@
+import { getPostBySlug, Post } from "@/lib/posts";
+import parser from "@/lib/parser";
+import style from "./page.module.sass";
+import Image from "next/image";
+
+interface ArticlePageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function ArticlePage({ params }: ArticlePageProps) {
+  const post = await getPostBySlug(params.slug);
+  const content = await parser(post.content);
+
+  if (!post) {
+    return <p>Post not found</p>;
+  }
+  return (
+      <article className={style.main_article}>
+        <aside>
+          <h1>{post.title}</h1>
+          <div className={style.author_infos}>
+            <Image src={post.author.picture} alt={`${post.author.name} image`} width={40} height={40} />
+            <p>{`written by ${post.author.name}`}</p>
+          </div>
+        </aside>
+        <section className={style.right_section}>
+          <figure>
+            <Image src={post.coverImage} alt={`${post.title}image`} width={100} height={100} sizes="100%"/>
+            <figcaption>Photo credit: {post.photoCredit?.cover ?? "Unknown"}</figcaption>
+          </figure>
+          <div dangerouslySetInnerHTML={{ __html: content }} className={style.article_text}/>
+        </section>
+      </article>
+  );
+}
