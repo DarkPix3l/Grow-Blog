8db618d50d4ec45020856a72e1b35d4f883863f2
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-09-11 17:36:23 +0200

    fix(NextArticle): wrapped each article in a link, added key and removed gap

--- src/app/_components/NextArticle/NextArticle.tsx
+++ src/app/_components/NextArticle/NextArticle.tsx
@@ -1,6 +1,7 @@
 import style from "./NextArticle.module.sass";
 import { getRandomPosts } from "@/lib/posts";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NextArticle({ currentSlug }: { currentSlug: string }) {
   const randomPost = getRandomPosts(2, currentSlug);
@@ -8,12 +9,13 @@ export default function NextArticle({ currentSlug }: { currentSlug: string }) {
     <div className={style.random_article_block}>
       <h3>You may also like</h3>
       {randomPost.map((post) => (
-        <div key={post.slug} className={style.snippet}>
+        <Link href={post.slug} key={post.slug}>
+        <div className={style.snippet}>
           <Image
             src={post.coverImage}
             width={120}
             height={120}
-            alt={post.slug}
+            alt={post.title}
             sizes="100%"
           />
           <div className={style.text_infos}>
@@ -21,6 +23,7 @@ export default function NextArticle({ currentSlug }: { currentSlug: string }) {
             <p>{post.excerpt}</p>
           </div>
         </div>
+        </Link>
       ))}
     </div>
   );
