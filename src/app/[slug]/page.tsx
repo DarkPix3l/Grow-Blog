3446719016cf7b7470044c4bfd1490c4d86c5a3a
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-10-26 10:50:28 +0100

    feat(ArticlePage): implement responsiveness
    - added mobile responsive layout with sliding section triggered by icon click
    - used CSS checkbox approach instead of refactoring component to client

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,81 +1,86 @@
-import { getPostBySlug } from "@/_lib/posts";
-import parser from "@/_lib/parser";
-import style from "./page.module.sass";
-import Image from "next/image";
-import InsetContainer from "../_components/_InsetContainer/InsetContainer";
-import NextArticle from "../_components/NextArticle/NextArticle";
-import { FiArrowLeftCircle } from "react-icons/fi";
-import Link from "next/link";
+import { getPostBySlug } from '@/_lib/posts'
+import parser from '@/_lib/parser'
+import style from './page.module.sass'
+import Image from 'next/image'
+import InsetContainer from '../_components/_InsetContainer/InsetContainer'
+import NextArticle from '../_components/NextArticle/NextArticle'
+import { FiArrowLeftCircle } from 'react-icons/fi'
+import Link from 'next/link'
+import { IoIosArrowDropupCircle } from "react-icons/io";
 
 interface ArticlePageProps {
-  params: {
-    slug: string;
-  };
+    params: {
+        slug: string
+    }
 }
 
 export default async function ArticlePage({ params }: ArticlePageProps) {
-  const { slug } = await params;
-  const post = await getPostBySlug(slug);
-  const content = await parser(post.content);
+    const { slug } = await params
+    const post = await getPostBySlug(slug)
+    const content = await parser(post.content)
 
-  if (!post) {
-    return <p>Post not found</p>;
-  }
-  return (
-    <main className={style.maincontainer}>
-      <InsetContainer>
-        <article className={style.main_article}>
-          <aside>
-            <div>
-              <h2>{post.title}</h2>
-              <div className={style.author_infos}>
-                <Image
-                  src={post.author.picture}
-                  alt={`${post.author.name} image`}
-                  width={40}
-                  height={40}
-                />
-                <p>{`written by ${post.author.name}`}</p>
-              </div>
-            </div>
-            <div>
-              <div className={style.deko}>
-                <div className={style.circle}></div>
-                <div className={style.line}></div>
-                <div className={style.circle}></div>
-              </div>
-              <NextArticle currentSlug={slug} />
-            </div>
-            <Link
-              href="/"
-              className={style.inset}>
-              <FiArrowLeftCircle size={32} />
-              <p>Back to Home</p>
-            </Link>
-          </aside>
-          <hr />
-          <section className={style.right_section}>
-            <figure>
-              <div className={style.img_mask}>
-              <Image
-                src={post.coverImage}
-                alt={`${post.title}image`}
-                width={100}
-                height={100}
-                sizes="100%"
-              />
-              </div>
-              <figcaption>
-                <em>Photo credit: {post.photoCredit?.cover ?? "Unknown"}</em>
-              </figcaption>
-            </figure>
-            <div
-              dangerouslySetInnerHTML={{ __html: content }}
-              className={style.article_text}
-            />
-          </section>
-        </article>
-      </InsetContainer>
-    </main>
-  );
+    if (!post) {
+        return <p>Post not found</p>
+    }
+    return (
+        <main className={style.maincontainer}>
+            <InsetContainer>
+                <article className={style.main_article}>
+                    <aside>
+                        <input type="checkbox" name="slideUp" id="slideUp" />
+                        <IoIosArrowDropupCircle size={50} className={style.arrowUp}/>
+
+                        <div>
+                            <h2>{post.title}</h2>
+                            <div className={style.author_infos}>
+                                <Image
+                                    src={post.author.picture}
+                                    alt={`${post.author.name} image`}
+                                    width={40}
+                                    height={40}
+                                />
+                                <p>{`written by ${post.author.name}`}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={style.deko}>
+                                <div className={style.circle}></div>
+                                <div className={style.line}></div>
+                                <div className={style.circle}></div>
+                            </div>
+                            <NextArticle currentSlug={slug} />
+                        </div>
+                        <Link href="/" className={style.inset}>
+                            <FiArrowLeftCircle size={32} />
+                            <p>Back to Home</p>
+                        </Link>
+                    </aside>
+                    <hr />
+                    <section className={style.right_section}>
+                        <figure>
+                            <div className={style.img_mask}>
+                                <Image
+                                    src={post.coverImage}
+                                    alt={`${post.title}image`}
+                                    width={100}
+                                    height={100}
+                                    sizes="100%"
+                                />
+                            </div>
+                            <figcaption>
+                                <em>
+                                    Photo credit:{' '}
+                                    {post.photoCredit?.cover ?? 'Unknown'}
+                                </em>
+                            </figcaption>
+                        </figure>
+                        <div
+                            dangerouslySetInnerHTML={{ __html: content }}
+                            className={style.article_text}
+                        />
+                    </section>
+                </article>
+            </InsetContainer>
+        </main>
+    )
 }
