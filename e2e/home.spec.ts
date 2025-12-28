98001ca199df529a95c54c1502ecd5843f0a2b28
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-12-28 13:01:36 +0100

    test: optimize e2e home flow and configure playwright webserver
    - Simplified article card check by removing loop.
    - Configured baseURL to use relative paths in tests.
    - Enabled webServer in config to automate local dev server startup.
    - disabled webkit "safari" test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,59 +1,47 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
 
-test("user visit the homepage", async ({ page }) => {
-  await page.setViewportSize({ width: 1920, height: 1080 });
+test('user visit the homepage', async ({ page }) => {
+    await page.setViewportSize({ width: 1920, height: 1080 })
 
-  await page.goto("http://localhost:3000", { timeout: 10000 });
+  await page.goto("/", { timeout: 10000 });
   expect(page).toBeTruthy();
 
-  // header
-  const logos = page.getByAltText("the Blog's Logo");
-  expect(logos).toHaveCount(2);
+    // header
+    const logos = page.getByAltText("the Blog's Logo")
+    expect(logos).toHaveCount(2)
 
-  await page.getByRole("link", { name: "link1" }).click();
-  await page.getByRole("link", { name: "link2" }).click();
-  await page.getByRole("link", { name: "link3" }).click();
-  await page.getByRole("link", { name: "Change language" }).click();
-  await page.getByRole("link", { name: "Toggle theme" }).click();
+    await page.getByRole('link', { name: 'link1' }).click()
+    await page.getByRole('link', { name: 'link2' }).click()
+    await page.getByRole('link', { name: 'link3' }).click()
+    await page.getByRole('link', { name: 'Change language' }).click()
+    await page.getByRole('link', { name: 'Toggle theme' }).click()
 
-  //Main title
-  const title = page.getByText("grow blog/_");
-  expect(title).toBeVisible();
+    //Main title
+    const title = page.getByText('grow blog/_')
+    expect(title).toBeVisible()
 
-  //description
-  const message = page.getByText("A blog platform built for the world.");
-  expect(message).toBeVisible();
+    //description
+    const message = page.getByText('A blog platform built for the world.')
+    expect(message).toBeVisible()
 
-  //CTA bar
-  await page.getByRole("link", { name: "start writing" }).click();
-  await page.getByRole("link", { name: "content your way" }).click();
+    //CTA bar
+    await page.getByRole('link', { name: 'start writing' }).click()
+    await page.getByRole('link', { name: 'content your way' }).click()
 
-  //Articles section h2
-  const latestPosts = page.getByText("Latest Posts");
-  expect(latestPosts).toBeVisible();
+    //Articles section h2
+    const latestPosts = page.getByText('Latest Posts')
+    expect(latestPosts).toBeVisible()
 
-  //article cards
-  const articleCards = page.getByRole("article");
-  expect(articleCards).toHaveCount(4);
+    //article cards
+    const articleCards = page.getByRole('article')
+    expect(articleCards).toHaveCount(4)
 
-  const count = await articleCards.count(); //returns number, not locator
+    // testing the first card about elements visibility
+    await expect(articleCards.getByTestId('categoryField').first()).toBeVisible()
+    await expect(articleCards.getByTestId('cardTitle').first()).toBeVisible()
+    await expect(articleCards.getByAltText('author picture').first()).toBeVisible()
+    await expect(articleCards.getByText('written by').first()).toBeVisible()
 
-  for (let i = 0; i < count; i++) {
-    const card = articleCards.nth(i);
-
-    const locators = [
-      card.getByTestId("categoryField"),
-      card.getByTestId("cardTitle"),
-      card.getByAltText("author picture"),
-      card.getByText("written by"),
-    ];
-
-    for (const locator of locators) {
-      await expect(locator).toBeVisible();
-    }
-  }
-
-  const firstLink = page.getByRole("link").filter({ hasText: "TechCreating Custom Fonts in" });
-  await firstLink.click();
-  await expect(page).toHaveURL("http://localhost:3000/custom-font");
-});
+    await page.getByRole('link').filter({ hasText: 'tech nextjs custom fonts' }).click()
+    await expect(page).toHaveURL('http://localhost:3000/creating-custom-fonts-in-next-js')
+})
