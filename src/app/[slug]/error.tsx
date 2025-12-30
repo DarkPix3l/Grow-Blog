1b5082ab29933b3663c4a56940310c3da2738bdc
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-12-30 08:55:21 +0100

    feat(error): changed uncaught exeptions error page to use next `redirect` instead of `reset`

--- src/app/[slug]/error.tsx
+++ src/app/[slug]/error.tsx
@@ -1,30 +1,35 @@
 'use client' // Error boundaries must be Client Components
- 
+
+import { redirect } from 'next/navigation'
 import { useEffect } from 'react'
- 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string }
-  reset: () => void
-}) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
-  return (
-    <div style={{display: "flex", flexDirection:"column", alignItems: "center", justifyContent: "center", height: "100vh", width:"100%" }}>
-      <h2>Something went wrong!</h2>
-      <button style={{padding:"0.5rem", borderRadius:"0.3rem", border:"1px solid braun"}}
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  )
+
+export default function Error({ error }: { error: Error & { digest?: string } }) {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error)
+    }, [error])
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+                width: '100%',
+            }}
+        >
+            <h2>Something went wrong!</h2>
+            <button
+                style={{ padding: '0.5rem', borderRadius: '0.3rem', border: '1px solid braun', cursor: 'pointer' }}
+                onClick={
+                    // changed to "back home" with default nextjs function
+                    () => redirect('/')
+                }
+            >
+                Try again
+            </button>
+        </div>
+    )
 }
