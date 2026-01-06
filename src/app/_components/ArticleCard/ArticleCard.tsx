8acd536f4938ce2705ae514f738da489a99d67ca
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-01-06 15:38:21 +0100

    feat(ArticleCard): made ArticleCard into a  flippable component
    - Implemented flip animation using CSS perspective and rotateY
    - Customized back-face content with other data, like post excerpt and cover image
    - Synced layout structure between front and back
    - Applied specific styling for back-side like lighter color for text, borders

---
+++ src/app/_components/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,64 @@
+import Image from 'next/image'
+import style from './ArticleCard.module.sass'
+import { FiArrowRightCircle } from 'react-icons/fi'
+import Link from 'next/link'
+import { MappedPost } from '@/types/types'
+
+export default function ArticleCard({
+  categories,
+  title,
+  authorPicture,
+  authorName,
+  slug,
+  coverImage,
+  excerpt,
+}: MappedPost) {
+  return (
+    <Link href={`/${slug}`} className={style.clickable}>
+      {/* FRONT */}
+      <article className={style.card}>
+        <div className={style.card_body}>
+          <div className={style.card_category}>
+            {categories.map((cat: string) => (
+              <p data-testid="categoryField" key={cat}>
+                {cat}
+              </p>
+            ))}
+          </div>
+          <div>
+            <h3 data-testid="cardTitle">{title}</h3>
+            <div className={style.author_infos}>
+              <Image src={authorPicture} alt={`${authorName}, author picture`} width={40} height={40} />
+              <p>{`written by ${authorName}`}</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.card_action}>
+          <p>go to article</p>
+          <FiArrowRightCircle size={32} />
+        </div>
+      </article>
+
+      {/* BACK */}
+      <article className={style.back}>
+        <div className={style.card_body}>
+          <div className={style.card_category}>
+            {categories.map((cat: string) => (
+              <p data-testid="categoryField" key={cat}>
+                {cat}
+              </p>
+            ))}
+          </div>
+          <div>
+            <p className={style.excerpt}>{excerpt}</p>
+          </div>
+        </div>
+        <Image src={coverImage} alt={`${title}, image`} fill />
+        <div className={style.card_action}>
+          <p>go to article</p>
+          <FiArrowRightCircle size={32} />
+        </div>
+      </article>
+    </Link>
+  )
+}
