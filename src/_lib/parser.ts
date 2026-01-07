a4675cd0d2f34f6daabf1b8cdb733f8313ed0a42
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-01-07 09:40:01 +0100

    chore(lib):  Remove legacy local file-based post fetching and markdown parsing logic

--- src/_lib/parser.ts
+++
@@ -1,7 +0,0 @@
-import { remark } from "remark";
-import html from "remark-html";
-
-export default async function parser(markdown: string): Promise<string> {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
-}
