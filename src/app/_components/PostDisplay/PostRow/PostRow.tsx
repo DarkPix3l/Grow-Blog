8212be4cca9f44adf5cc2707e22f60d4caa1ccdc
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-01-08 19:50:44 +0100

    Feat(PostRow): add PostRow component
    - Displays a row of posts with a title and vertical divider.

---
+++ src/app/_components/PostDisplay/PostRow/PostRow.tsx
@@ -0,0 +1,27 @@
+// PostRow.tsx
+import { Divider } from '../../Ui/Divider/Divider'
+import ArticleCard from '../ArticleCard/ArticleCard'
+import style from './PostRow.module.sass'
+import { MappedPosts } from '@/types/types'
+
+interface PostRowProps {
+  title: string
+  posts: MappedPosts
+}
+
+export default function PostRow({ title, posts }: PostRowProps) {
+  return (
+    <div className={style.row}>
+      <div className={style.labelSide}>
+        <h3>{title}</h3>
+        <Divider orientation="vertical" flexDir="column" deko={true} />
+      </div>
+      
+      <div className={style.cardsSide}>
+        {posts.map((post) => (
+          <ArticleCard key={post.slug} {...post} variant="static" layout="mini" />
+        ))}
+      </div>
+    </div>
+  )
+}
