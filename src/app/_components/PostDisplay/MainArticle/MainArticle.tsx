083c8ddebd2fd47dc798ae2397acf09159caf6f5
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-01-17 18:45:52 +0100

    feat(MainArticle): implement syntax highlighting for article code blocks
    - Add syntax-highlighter component with a11yDark theme, language and custom styling.
    - Added a conditional check in Contentful's renderNode to intercept paragraphs containing code; these are now rendered as div elements to prevent hydration mismatches caused by invalid HTML nesting.
    - add options in documentToReactComponents

--- src/app/_components/PostDisplay/MainArticle/MainArticle.tsx
+++ src/app/_components/PostDisplay/MainArticle/MainArticle.tsx
@@ -3,11 +3,47 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Image from 'next/image'
 import style from './MainArticle.module.sass'
 import ErrorComp from '../../NotFoundComponent/NotFoundComponent'
+import { MARKS, BLOCKS, NodeData } from '@contentful/rich-text-types'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 export default async function MainArticle({ post }: MainArticleProps) {
   // internal error handling
   if (!post) {
     return <ErrorComp imgH={150} imgW={150} errorMessage="The article could not be found." />
+  }
+
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node: NodeData, children: React.ReactNode) => {
+        if (node.content.length === 1 && node.content[0].marks.find((x: NodeData) => x.type === MARKS.CODE)) {
+          return <div>{children}</div>
+        }
+        return <p>{children}</p>
+      },
+    },
+    renderMark: {
+      [MARKS.CODE]: (text: React.ReactNode) => {
+        return (
+          <SyntaxHighlighter
+            language="javascript"
+            style={a11yDark}
+            wrapLines={true}
+            lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
+            customStyle={{
+              margin: '1.5em 0',
+              padding: '1em',
+              borderRadius: '8px',
+              fontSize: '0.9rem',
+              lineHeight: '1.5',
+              backgroundColor: '#393131',
+            }}
+          >
+            {String(text)}
+          </SyntaxHighlighter>
+        )
+      },
+    },
   }
 
   return (
@@ -28,7 +64,7 @@ export default async function MainArticle({ post }: MainArticleProps) {
           <em>{post.photoCredit ?? ''}</em>
         </figcaption>
       </figure>
-      <div className={style.article_text}>{documentToReactComponents(post.content)}</div>
+      <div className={style.article_text}>{documentToReactComponents(post.content, options)}</div>
     </article>
   )
 }
