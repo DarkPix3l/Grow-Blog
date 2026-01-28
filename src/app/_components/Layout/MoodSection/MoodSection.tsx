76f5ff19512605816c18826d3ce4d1f8669ff5f6
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-01-28 15:45:37 +0100

    refactor(MoodSection): extract mood section from Home page

---
+++ src/app/_components/Layout/MoodSection/MoodSection.tsx
@@ -0,0 +1,31 @@
+import style from './MoodSection.module.sass'
+import PostRow from '@/app/_components/PostDisplay/PostRow/PostRow'
+import Section from '@/app/_components/Layout/Section/Section'
+import TextBody from '@/app/_components/Ui/TextBody/TextBody'
+import FlexWrapper from '@/app/_components/Ui/FlexWrapper/FlexWrapper'
+import WeatherWidget from '@/app/_components/Ui/WeatherWidget/WeatherWidget'
+import InsetContainer from '@/app/_components/Layout/InsetContainer/InsetContainer'
+import { MappedPosts } from '@/types/types'
+
+interface MoodSectionProps {
+  weatherPosts: MappedPosts
+}
+export default function MoodSection({ weatherPosts }: MoodSectionProps) {
+  return (
+    <Section id="mood-section" className={style.mood_section}>
+      <InsetContainer variant="pure" className={style.moodContainer}>
+        <FlexWrapper className="flex_center">
+          <h2>Weather mood Pickup</h2>
+          <TextBody>
+            Whether it's a rainy day for deep thinking or a bright morning for quick inspiration, we’ve got the right
+            story for your mood.
+          </TextBody>
+        </FlexWrapper>
+        <PostRow posts={weatherPosts} variant="static" layout="compact" ContainerClassName={style.mood_row} />
+      </InsetContainer>
+      <InsetContainer variant="pure" className={style.weatherContainer}>
+        <WeatherWidget />
+      </InsetContainer>
+    </Section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 //Main_Page
 
-import styles from './page.module.sass'
-import InsetContainer from './_components/Layout/InsetContainer/InsetContainer'
 import { getLatestPost, getPostsByCategory, mapWeatherToCategory } from '@/_lib/contentful-posts'
 import { mapPost } from '@/types/types'
-import PostRow from './_components/PostDisplay/PostRow/PostRow'
-import Section from './_components/Layout/Section/Section'
-import TextBody from './_components/Ui/TextBody/TextBody'
-import FlexWrapper from './_components/Ui/FlexWrapper/FlexWrapper'
+// import styles from './page.module.sass'
 import Footer from './_components/Layout/Footer/Footer'
-import WeatherWidget from './_components/Ui/WeatherWidget/WeatherWidget'
 import IntroSection from './_components/Layout/IntroSection/IntroSection'
 import CategoriesSection from './_components/Layout/CategoriesSection/CategoriesSection'
+import MoodSection from './_components/Layout/MoodSection/MoodSection'
 
 export default async function Home() {
   const latestPosts = (await getLatestPost()).items.map(mapPost)
@@ -28,22 +23,7 @@ export default async function Home() {
       <main>
         <IntroSection posts={latestPosts} />
         <CategoriesSection techPosts={techPosts} nextjsPosts={nextjsPosts} careerPosts={careerPosts} />
-
-        <Section id="mood-section" className={styles.mood_section}>
-          <InsetContainer variant="pure" className={styles.moodContainer}>
-            <FlexWrapper className="flex_center">
-              <h2>Weather mood Pickup</h2>
-              <TextBody>
-                Whether it's a rainy day for deep thinking or a bright morning for quick inspiration, we’ve got the
-                right story for your mood.
-              </TextBody>
-            </FlexWrapper>
-            <PostRow posts={weatherPosts} variant="static" layout="compact" ContainerClassName={styles.mood_row} />
-          </InsetContainer>
-          <InsetContainer variant="pure" className={styles.weatherContainer}>
-            <WeatherWidget />
-          </InsetContainer>
-        </Section>
+        <MoodSection weatherPosts={weatherPosts} />
       </main>
       <Footer />
     </>
