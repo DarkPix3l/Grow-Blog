59ca2ebb249f244627a0f64e0892b4c6d8552db6
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-01-29 10:44:31 +0100

    fix(CtaBar): remove viewport-based conditional rendering hook
    - eliminate hydration flicker caused by delayed screen checks
    - hide CTA bar on mobile via media queries
    - Turn component into server

--- src/app/_components/Layout/CTAbar/CtaBar.tsx
+++ src/app/_components/Layout/CTAbar/CtaBar.tsx
@@ -1,37 +1,25 @@
-"use client"
-import style from "./CtaBar.module.sass";
-import Image from "next/image";
-import Link from "next/link";
-import { FiArrowUpRight } from "react-icons/fi";
-import { useConditionalRendering } from "@/_hooks/ConditionalRendering";
+import style from './CtaBar.module.sass'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FiArrowUpRight } from 'react-icons/fi'
 
 export default function CtaBar() {
-
-  const isMobile = useConditionalRendering("(max-width: 767px)")
-  if(isMobile) return null
-
   return (
     <section className={style.ctaBar}>
-      <Image
-        className={style.ctaLogo}
-        src="/img/logo.svg"
-        alt="the Blog's Logo"
-        width={40}
-        height={40}
-      />
-      <Link href={"/"} aria-label="start writing">
+      <Image className={style.ctaLogo} src="/img/logo.svg" alt="the Blog's Logo" width={40} height={40} />
+      <Link href={'/'} aria-label="start writing">
         <h2>
           <strong>Start Writing!</strong>
         </h2>
         <FiArrowUpRight size={43} />
       </Link>
       <div className={style.tratto}></div>
-      <Link href={"/"} aria-label="content your way">
+      <Link href={'/'} aria-label="content your way">
         <h2>
           <strong>Content, Your Way</strong>
         </h2>
         <FiArrowUpRight size={43} />
       </Link>
     </section>
-  );
+  )
 }
