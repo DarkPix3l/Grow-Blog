efbb866ae44aa0141a0ccf55881c3b678a0ff137
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-02-09 18:00:00 +0100

    feat(dark-light-theme): add a few variables from the design and temporarily replace data-theme attribute with hardcoded value

--- src/_hooks/CookieGetter.tsx
+++ src/_hooks/CookieGetter.tsx
@@ -5,8 +5,8 @@ export default async function CookieGetter({ children, className}: { children: R
       const cookieStore = await cookies()
       const theme = cookieStore.get('theme')?.value || 'light'
       // Create the class string based on the cookie value
-      const themeClass = theme === 'dark' ? 'theme-dark' : 'theme-light'
+      // const themeClass = theme === 'dark' ? 'theme-dark' : 'theme-light'
   return (
-    <body data-theme={themeClass} className={className}>{children}</body>
+    <body data-theme={'theme-light'} className={className}>{children}</body>
   )
 }
