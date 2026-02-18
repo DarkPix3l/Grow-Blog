c7173d13bae39062a7653addab9df1cffa2da696
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2026-02-18 12:59:21 +0100

    test(intoSection.spec): move intro section tests in a separate file and add description

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -5,64 +5,6 @@ test.describe('Homepage Content & Navigation', () => {
     await page.goto('/')
   })
 
-  test('it should display Intro Section content @desktop', async ({ page }) => {
-    //Intro Section
-    const introSection = page.locator('#intro_section')
-    //Main title
-    const mainTitle = introSection.getByText('grow blog/_')
-    await expect(mainTitle).toBeVisible()
-
-    //description
-    const message = introSection.getByText('A blog platform built for the world.')
-    await expect(message).toBeVisible()
-
-    //CTA bar
-    const startWriting = introSection.getByRole('link', { name: 'start writing' })
-    const contentYourWay = page.getByRole('link', { name: 'content your way' })
-    await expect(startWriting).toBeVisible()
-    await expect(contentYourWay).toBeVisible()
-
-    //Articles section h2
-    const latestPostsHeading = introSection.getByRole('heading', { name: 'Latest Posts' })
-    await expect(latestPostsHeading).toBeVisible()
-    await expect(latestPostsHeading).toContainText('Latest Posts')
-
-    const paragraph = introSection.getByText('A blog platform built for the world')
-    await expect(paragraph).toBeVisible()
-
-    //article cards
-    //Unfortunately also the number of the cards can change. Getting the first
-    const firstCardBody = page.getByRole('article').first()
-
-    // testing the first card about elements visibility
-    await expect(firstCardBody.getByTestId('categoryField').first()).toBeVisible()
-    await expect(firstCardBody.getByTestId('cardTitle').first()).toBeVisible()
-    await expect(firstCardBody.getByAltText('author picture').first()).toBeVisible()
-    await expect(firstCardBody.getByText('written by').first()).toBeVisible()
-  })
-
-  test('first cta link should be clickable @desktop', async ({ page }) => {
-    const introSection = page.locator('#intro_section')
-    await introSection.getByRole('link', { name: 'start writing' }).click() //page not yet implemented
-  })
-
-  test('second cta link should go to about page @desktop', async ({ page }) => {
-    const introSection = page.locator('#intro_section')
-    const contentYourWay = introSection.getByRole('link', { name: 'content your way' })
-    await contentYourWay.click()
-    await expect(page).toHaveURL('/about')
-  })
-
-  test('Article Card should be clickable @desktop', async ({ page }) => {
-    const firstCard = page.getByTestId('cardLink').first()
-
-    // I'm replacing the previous link test because the latest posts will always change.
-    // Going for a more content-agnostic approach, more suitable for this CMS's "living content."
-    const href = await firstCard.getAttribute('href')
-    await firstCard.click()
-    await expect(page).toHaveURL(new RegExp(`${href}`))
-  })
-
   test('it should display Categories Section content @desktop', async ({ page }) => {
     const categoriesSection = page.locator('#categories_section')
 
